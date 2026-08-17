db8b6c8795001c654ac13090e2dd454e7ae4b245
Karthik S <karthiksaya14@gmail.com>
2026-08-17 20:42:19 +0530

    Remove standalone evaluation package

--- evaluation/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Lightweight evaluation harness package."""
-
-from evaluation.harness import EvaluationHarness
-from evaluation.models import EvaluationReport, EvaluationResult, EvaluationTask
-
-__all__ = ["EvaluationHarness", "EvaluationReport", "EvaluationResult", "EvaluationTask"]
